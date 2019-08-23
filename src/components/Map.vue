95df5b0f5173bbf0b9164ca2162b097ac137599b
ericall <ericallhans@gmail.com>
2019-08-23 17:48:49 -0500

    adjusting widths with results

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="viewDiv" 
-    v-bind:style="{height: mapHeight + 'px' }"
-    v-bind:class="{ minMap: minimizeMap }"></div>
+  <div
+    id="viewDiv"
+    v-bind:style="{height: mapHeight + 'px', width: mapWidth, marginLeft: mapLeftMargin }"
+  ></div>
 </template>
 
 <script>
@@ -15,23 +16,24 @@ const esriBasemapOptions = {
   center: [-93.5, 45]
 };
 
-
 export default {
   name: "Header",
   data() {
     return {
       mapHeight: 0,
+      mapWidth: "100%",
+      mapLeftMargin: 0,
       esriMods: {},
       identifyResults: [],
       minimizeMap: false
     };
   },
-    computed: {
+  computed: {
     ...mapGetters({
       result: "getResults"
     })
   },
-    watch: {
+  watch: {
     result(res) {
       console.log("watch result", res);
 
@@ -40,20 +42,19 @@ export default {
   },
   methods: {
     processResults(res) {
-    this.minimizeMap =true;
+      this.minimizeMap = true;
     },
     handleResize() {
       var headerHeight = document.getElementsByTagName("header")[0]
         .clientHeight;
       this.mapHeight = window.innerHeight - headerHeight;
+      this.adjustMapWidth();
     },
 
     executeIdentifyTask(event) {
       let _this = this;
       // Create identify task for the specified map service
-      let identifyTask = new this.esriMods.IdentifyTask(
-        config.MetroGISUrl
-      );
+      let identifyTask = new this.esriMods.IdentifyTask(config.MetroGISUrl);
 
       // Set the parameters for the Identify
       let params = new this.esriMods.IdentifyParameters();
@@ -68,9 +69,40 @@ export default {
       identifyTask.execute(params).then(function(response) {
         var results = response.results;
         console.log(results);
-        _this.identifyResults = results;
-         _this.$store.commit('setNewResult', results)
+        if (results.length > 0) {
+          _this.identifyResults = results;
+          _this.$store.commit("setNewResult", results);
+          _this.adjustMapWidth("results");
+        } else {
+          alert("no results");
+        }
       });
+    },
+
+    adjustMapWidth(type) {
+      let windowWidth =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+
+      if (type !== "clear") {
+        if (windowWidth > 1400) {
+          this.mapWidth = "70%";
+          this.mapLeftMargin = "30%";
+        } else if (windowWidth > 900 && windowWidth <= 1400) {
+          this.mapWidth = "60%";
+          this.mapLeftMargin = "40%";
+        } else if (windowWidth > 700 && windowWidth <= 900) {
+          this.mapWidth = "50%";
+          this.mapLeftMargin = "50%";
+        } else if (windowWidth <= 700) {
+          this.mapWidth = "100%";
+          this.mapLeftMargin = "0";
+        }
+      } else {
+        this.mapWidth = "100%";
+        this.mapLeftMargin = "0";
+      }
     }
   },
   mounted() {
@@ -118,13 +150,23 @@ export default {
         };
         // create map with the given options at a DOM node w/ id 'mapNode'
         var mapLayer = new MapImageLayer({
-          url:
-            //  "http://gis.hennepin.us/arcgis/rest/services/Maps/ADOPT_A_HIGHWAY/MapServer",
-            config.MetroGISUrl,
+          url: config.MetroGISUrl,
           sublayers: [
             {
               id: 1,
-              visible: true
+              visible: true,
+              renderer: {
+                type: "simple", // autocasts as new SimpleRenderer()
+                symbol: {
+                  type: "simple-fill", // autocasts as new SimpleFillSymbol()
+                  color: [255, 128, 0, 0],
+                  outline: {
+                    // autocasts as new SimpleLineSymbol()
+                    width: 1,
+                    color: "purple"
+                  }
+                }
+              }
             }
           ]
         });
@@ -143,7 +185,7 @@ export default {
 
         config.View.when(function() {
           // executeIdentifyTask() is called each time the view is clicked
-          config.View.on("click", _this.executeIdentifyTask)
+          config.View.on("click", _this.executeIdentifyTask);
           //   let identifyTask = new IdentifyTask(
           //     "https://gis2.metc.state.mn.us/arcgis/rest/services/MetroGIS/Land_Ownership/MapServer"
           //   );
@@ -163,7 +205,7 @@ export default {
           //     console.log(results);
           //   });
           // }
-         // );
+          // );
         });
       }
     );
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,10 +1,11 @@
 <template>
-  <div v-bind:class="{ resultsOpen: showLeftPane }" >
+  <div 
+    v-bind:class="{ resultsOpen: showLeftPane }"
+     v-bind:style="{width: resultsWidth}">
     <table class="table">
-
       <tbody>
+        <tr></tr>
         <tr>
-            <tr>
           <th scope="row">PIN</th>
           <td>{{addressResult.pin}}</td>
         </tr>
@@ -34,13 +35,17 @@ export default {
   name: "Results",
   data() {
     return {
+      resultsWidth: 0,
       nresult: null,
       showLeftPane: false,
-     addressResult: {
-        owner:null,
+      addressResult: {
+        owner: null,
         pid: null
       }
     };
+  },
+  mounted() {
+     window.addEventListener("resize", this.adjustLeftPaneWidth);
   },
   computed: {
     // result() {
@@ -59,14 +64,36 @@ export default {
   },
   methods: {
     processResults(res) {
-        this.showLeftPane = true;
+      this.showLeftPane = true;
       this.nresult = res;
-      if (res.length === 1){
+      if (res.length === 1) {
         this.processAttributes(res[0].feature.attributes);
+        this.adjustLeftPaneWidth("results");
       }
     },
 
-    processAttributes(attr){
+    adjustLeftPaneWidth(type) {
+      let windowWidth =
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+
+      if (type !== "clear") {
+        if (windowWidth > 1400) {
+          this.resultsWidth = "30%";
+        } else if (windowWidth > 900 && windowWidth <= 1400) {
+          this.resultsWidth = "40%";
+        } else if (windowWidth > 700 && windowWidth <= 900) {
+          this.resultsWidth = "50%";
+        } else if (windowWidth <= 700) {
+          this.resultsWidth = "100%";
+        }
+      } else {
+        this.resultsWidth = "0";
+      }
+    },
+
+    processAttributes(attr) {
       this.addressResult.owner = attr.OWNER_NAME;
       this.addressResult.pin = attr.COUNTY_PIN;
       this.addressResult.streetAddress = this.constructStreetAddress(attr);
@@ -74,20 +101,19 @@ export default {
 
     constructStreetAddress(attr) {
       const houseNumber = attr.ANUMBER;
-
     }
-
   }
 };
 </script>
 
 <style scoped>
-.resultsOpen{
-    width: 350px;
-    height: 100%;
-    left: 0;
-    position: absolute;
-    top: 0;
-    background: white;
+.resultsOpen {
+  width: 30%;
+  height: 100%;
+  left: 0;
+  position: absolute;
+  top: 0;
+  background: white;
+  margin-top: 56px;
 }
 </style>
