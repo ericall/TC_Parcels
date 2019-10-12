857a4ae14c74138cf1fed0ac339ea1a14f824953
ericall <ericallhans@gmail.com>
2019-10-12 18:28:01 -0500

    adding material design

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store/store'
 import './registerServiceWorker'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
